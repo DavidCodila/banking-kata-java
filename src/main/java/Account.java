a4973e5d7027533e91fc922d34ca69aadafe8b95
David Codila <david.codila@auspost.com.au>
2025-02-21 09:35:53 +1100

    add passing code for deposit tests

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,11 +1,15 @@
 public class Account {
     private int balance;
 
+    public Account() {
+        this.balance = 0;
+    }
+
     public int getBalance() {
         return balance;
     }
 
     public void deposit(int amount) {
-
+        this.balance += amount;
     }
 }
--- src/test/java/TestAccount.java
+++ src/test/java/TestAccount.java
@@ -1,12 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAccount {
+    private Account account;
+
+    @BeforeEach
+    public void setup() {
+        account = new Account();
+    }
+
     @Test
     public void testAccountDeposit_1() {
-        Account account = new Account();
         account.deposit(1);
         assertEquals(1, account.getBalance());
+    }
+
+    @Test
+    public void testAccountDeposit_0() {
+        account.deposit(0);
+        assertEquals(0, account.getBalance());
     }
 }
