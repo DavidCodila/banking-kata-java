2626368a283f8930bb41f1c06feb041e13041d54
David Codila <david.codila@auspost.com.au>
2025-02-24 10:59:04 +1100

    add example in main and fix formatting in statementHeader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,13 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Transaction transaction = new Transaction(2, 1, Constants.withdrawalType, LocalDate.now());
-        System.out.println(transaction.getInformation());
+        LocalDate date = LocalDate.now();
+        Account account = new Account();
+        account.deposit(1000, date);
+        account.deposit(200, date);
+        date = date.plusDays(100);
+        account.withdraw(300, date);
+        account.withdraw(100, date);
+        System.out.println(account.printStatement());
     }
 }
