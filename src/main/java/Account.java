d17744194052ff1841c0eadb9dd14a028b2dc55c
David Codila <david.codila@auspost.com.au>
2025-02-28 17:11:13 +1100

    refactor add classes and remove unneeded classes

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,47 +2,67 @@ import java.security.InvalidParameterException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Account {
-    private int balance;
-    private final List<Transaction> transactions;
-    private final TransactionFilter filter;
-
-    public Account(TransactionFilter filter) {
-        this.balance = 0;
-        this.transactions = new ArrayList<>();
-        this.filter = filter;
-    }
+    private int balance = 0;
+    private final List<Transaction> transactions = new ArrayList<>();
 
     public int getBalance() {
         return this.balance;
     }
 
     public void deposit(int amount, LocalDate date) throws InvalidParameterException {
-        ValidateTransaction.validateDeposit(amount);
+        if (amount <= 0) {
+            throw new InvalidParameterException("Can not deposit less than 1");
+        }
         this.balance += amount;
         transactions.add(new Transaction(amount, this.balance, TransactionType.DEPOSIT, date));
     }
 
     public void withdraw(int amount, LocalDate date) throws RuntimeException {
-        ValidateTransaction.validateWithdrawal(amount, this.balance);
+        this.validateWithdrawal(amount);
         this.balance -= amount;
         this.transactions.add(new Transaction(amount, this.balance, TransactionType.WITHDRAWAL, date));
     }
 
     public String printStatement() {
-        this.filter.loadTransactions(this.transactions);
-        return Constants.statementHeader + this.filter.getAllTransactions();
+        return this.filteredTransactions(transaction -> true);
     }
 
-    public String getTransactionsByAmount(int amount) {
-        this.filter.loadTransactions(this.transactions);
-        return Constants.statementHeader + this.filter.getTransactionsByAmount(amount);
+    public String printStatementByAmount(int amount) {
+        return this.filteredTransactions(transaction -> transaction.amount() == amount);
     }
 
-    public String getTransactionsByDate(LocalDate date) {
-        this.filter.loadTransactions(this.transactions);
-        return Constants.statementHeader + this.filter.getTransactionsByDate(date);
+    public String printStatementByDate(LocalDate date) {
+        return this.filteredTransactions(transaction -> transaction.date() == date);
+    }
+
+    public String printWithdrawalTransactions() {
+        return this.filteredTransactions(transaction -> transaction.type() == TransactionType.WITHDRAWAL);
+    }
+
+    public String printDepositTransactions() {
+        return this.filteredTransactions(transaction -> transaction.type() == TransactionType.DEPOSIT);
+    }
+
+    private String filteredTransactions(Predicate<Transaction> streamPredicate) {
+        return Constants.statementHeader +
+                this.transactions.stream()
+                        .filter(streamPredicate)
+                        .map(transaction -> transaction.type().getFormattedTransaction(transaction))
+                        .collect(Collectors.joining())
+                        .stripTrailing();
+    }
+
+    private void validateWithdrawal(int amount) {
+        if (amount <= 0) {
+            throw new InvalidParameterException("Can not withdraw less than 1");
+        }
+        if (this.balance - amount < 0) {
+            throw new RuntimeException("Invalid funds to make transaction");
+        }
     }
 
 }
